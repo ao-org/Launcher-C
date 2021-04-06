70a4ca66afffdc836960438b32cc6611810dbc24
Joaquin <jopiodz00@gmail.com>
2021-04-06 14:12:02 -0300

    no se usa

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,16 +12,5 @@ namespace Launcher
     {
         // Todos los archivos del cliente en la subcarpeta Argentum20, para no mezclarlos con los archivos del Launcher.
         public static string ARGENTUM_FILES = Directory.GetCurrentDirectory() + "\\Argentum20\\";
-        
-        public App()
-        {
-            // Si detectamos otra instancia de la aplicación, la cerramos.
-            _ = new Mutex(true, @"Global\WinterAO_Launcher", out bool onlyInstance);
-            if (!onlyInstance)
-            {
-                MessageBox.Show("El launcher ya esta abierto.", "Error", MessageBoxButton.OK);
-                Environment.Exit(0);
-            }
-        }
     }
 }
